c04d9ceb32fca641b60be277a538c86b4cbfdf5a
EmanuelRdzM <j.emanuel.rdz.m@hotmail.com>
2026-02-14 17:05:39 -0600

    instalacion de vuetify

--- src/main.js
+++ src/main.js
@@ -5,7 +5,24 @@ import './styles/theme.css'
 import router from './router'
 import { createPinia } from 'pinia'
 
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { es } from 'vuetify/locale'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  locale: {
+    locale: 'es',
+    fallback: 'en',
+    messages: { es }
+  }
+})
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
